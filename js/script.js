d6959fe1227133ea8729a94b2d4a811973843ee1
AndresEcheverriFranco <67934044+AndresEcheverriFranco@users.noreply.github.com>
2021-09-30 20:52:36 -0500

    Se sube desafio 3 crear algoritmo con ciclos - 3 ejemplos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* for (i = 0; i < 5; i++) {
+    let number = Number(prompt("Please type a number"))
+    console.log("The sumatory of " + number + " and " + number + " is " + (number + number));
+} */
+
+/* let password = "";
+while (password != "1234") { password = prompt("Please type de correct password", "4 digits only") };
+alert("Correct!!! You can access now!") */
+
+/* let number = Number(prompt("Type a number of repetitions"));
+for (let i = 0; i < number; i++) { console.log("Hello, how are you??") } */
